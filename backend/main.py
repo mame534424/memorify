bf0554b44e8435f824a02744ed90d9c3c79d8024
Mohammed Abrar <mame.534424@gmail.com>
2026-06-26 10:12:03 +0300

    refractor: working of supabase connection

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from database import engine
-
+from sqlalchemy import text
 
 app = FastAPI()
 
@@ -10,4 +10,17 @@ def home():
 
     return {
         "message": "AI Assistant Backend Running"
+    }
+    
+@app.get("/db-test")
+def database_test():
+
+    with engine.connect() as connection:
+
+        result = connection.execute(
+            text("SELECT 1")
+        )
+
+    return {
+        "database": "connected"
     }
